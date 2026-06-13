f4973bca6d79f772dbe35d5fbb560674fa0d0b4c
Widthdom <widthdom@gmail.com>
2026-06-14 02:34:29 +0900

    Fix trimmed query publish lock file output (#3619)

--- tests/CodeIndex.Tests/QueryCommandRunnerTests.cs
+++ tests/CodeIndex.Tests/QueryCommandRunnerTests.cs
@@ -5364,6 +5364,8 @@ public partial class QueryCommandRunnerTests
         Directory.CreateDirectory(outputDir);
         var buildOutputDir = Path.Combine(outputDir, "bin", "publish") + Path.DirectorySeparatorChar;
         var intermediateDir = Path.Combine(outputDir, "obj", "publish") + Path.DirectorySeparatorChar;
+        Directory.CreateDirectory(intermediateDir);
+        var lockFilePath = Path.Combine(intermediateDir, "packages.lock.json");
 
         var psi = new System.Diagnostics.ProcessStartInfo("dotnet")
         {
@@ -5386,6 +5388,7 @@ public partial class QueryCommandRunnerTests
         psi.ArgumentList.Add("-p:PublishSingleFile=false");
         psi.ArgumentList.Add($"-p:OutputPath={buildOutputDir}");
         psi.ArgumentList.Add($"-p:IntermediateOutputPath={intermediateDir}");
+        psi.ArgumentList.Add($"-p:NuGetLockFilePath={lockFilePath}");
         psi.ArgumentList.Add("-p:UseSharedCompilation=false");
 
         using var process = System.Diagnostics.Process.Start(psi)
