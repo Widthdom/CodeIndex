0f958be37d6b953ebb9e7ed3f2f23f6e9a491acb
Widthdom <widthdom@gmail.com>
2026-06-25 22:02:32 +0900

    Avoid Fortran module procedure end trim chain

--- src/CodeIndex/Indexer/Symbols/SymbolExtractor.Fortran.cs
+++ src/CodeIndex/Indexer/Symbols/SymbolExtractor.Fortran.cs
@@ -285,7 +285,7 @@ public static partial class SymbolExtractor
         if (!StartsWithFortranWord(trimmedLine, "end"))
             return false;
 
-        var remainder = trimmedLine["end".Length..].TrimStart();
+        var remainder = GetFortranSuffixTrimmedStart(trimmedLine, "end".Length);
         if (!StartsWithFortranWord(remainder, "module"))
             return false;
 
