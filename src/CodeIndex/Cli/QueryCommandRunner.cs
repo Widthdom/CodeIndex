9a12c08a0144a2e6e42cca990343da799b42e2c6
Widthdom <widthdom@gmail.com>
2026-07-01 16:35:34 +0900

    Extract query batch limit constants

--- src/CodeIndex/Cli/QueryCommandRunner.cs
+++ src/CodeIndex/Cli/QueryCommandRunner.cs
@@ -33,11 +33,6 @@ public static partial class QueryCommandRunner
     internal const int MaxWorkspaceDependencyDatabasePairCount = MaxWorkspaceDependencyDatabaseCount * (MaxWorkspaceDependencyDatabaseCount - 1);
     internal const int FindAllCandidateFileLimit = 4096;
     internal const int FindAllLineScanLimit = 250_000;
-    internal const int BatchMaxLineChars = 1024 * 1024;
-    internal const int BatchMaxLineUtf8Bytes = BatchMaxLineChars * 4;
-    internal const int BatchMaxArgumentCount = 256;
-    internal const int BatchMaxArgumentChars = 8192;
-    internal const int BatchMaxJsonDepth = 32;
     internal const int MaxStatusSymbolKindEntries = 32;
     internal const int MaxStatusSymbolKindNameLength = 64;
     private const int MaxSearchProjectionFieldsCsvLength = 256;
