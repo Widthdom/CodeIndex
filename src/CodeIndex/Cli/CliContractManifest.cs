5cddf7f803d5deff94891f14d1825ab605a239c2
Widthdom <widthdom@gmail.com>
2026-07-17 00:54:43 +0900

    Register structured query error contracts (#4564)

--- src/CodeIndex/Cli/CliContractManifest.cs
+++ src/CodeIndex/Cli/CliContractManifest.cs
@@ -54,6 +54,9 @@ internal static class CliContractManifest
         new(nameof(CommandErrorCodes.FileSystemCaseProbeFailed), CommandErrorCodes.FileSystemCaseProbeFailed, CommandExitCodes.DatabaseError),
         new(nameof(CommandErrorCodes.CheckpointNotFound), CommandErrorCodes.CheckpointNotFound, CommandExitCodes.NotFound),
         new(nameof(CommandErrorCodes.WorkspaceManifestInvalid), CommandErrorCodes.WorkspaceManifestInvalid, null),
+        new(nameof(CommandErrorCodes.QueryNotFound), CommandErrorCodes.QueryNotFound, CommandExitCodes.NotFound),
+        new(nameof(CommandErrorCodes.FileNotFound), CommandErrorCodes.FileNotFound, CommandExitCodes.NotFound),
+        new(nameof(CommandErrorCodes.LineOutOfRange), CommandErrorCodes.LineOutOfRange, CommandExitCodes.InvalidArgument),
     ];
 
     public static IReadOnlyList<Type> CliJsonRootTypes { get; } = LoadCliJsonRootTypes();
